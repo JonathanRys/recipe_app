cef4f2fc653084f9bf095719a14a78f3159e6fd8
JonathanRys <jonathan.rk.rys@gmail.com>
2023-04-14 13:03:48 -0400

    add missing file

---
+++ recipe_app/data/language_codes.py
@@ -0,0 +1,742 @@
+language_codes = {
+    "ab": {
+        "group": "Northwest Caucasian",
+        "language": "Abkhazian"
+    },
+    "aa": {
+        "group": "Afro-Asiatic",
+        "language": "Afar"
+    },
+    "af": {
+        "group": "Indo-European",
+        "language": "Afrikaans"
+    },
+    "ak": {
+        "group": "Niger–Congo",
+        "language": "Akan"
+    },
+    "sq": {
+        "group": "Indo-European",
+        "language": "Albanian"
+    },
+    "am": {
+        "group": "Afro-Asiatic",
+        "language": "Amharic"
+    },
+    "ar": {
+        "group": "Afro-Asiatic",
+        "language": "Arabic"
+    },
+    "an": {
+        "group": "Indo-European",
+        "language": "Aragonese"
+    },
+    "hy": {
+        "group": "Indo-European",
+        "language": "Armenian"
+    },
+    "as": {
+        "group": "Indo-European",
+        "language": "Assamese"
+    },
+    "av": {
+        "group": "Northeast Caucasian",
+        "language": "Avaric"
+    },
+    "ae": {
+        "group": "Indo-European",
+        "language": "Avestan"
+    },
+    "ay": {
+        "group": "Aymaran",
+        "language": "Aymara"
+    },
+    "az": {
+        "group": "Turkic",
+        "language": "Azerbaijani"
+    },
+    "bm": {
+        "group": "Niger–Congo",
+        "language": "Bambara"
+    },
+    "ba": {
+        "group": "Turkic",
+        "language": "Bashkir"
+    },
+    "eu": {
+        "group": "Language isolate",
+        "language": "Basque"
+    },
+    "be": {
+        "group": "Indo-European",
+        "language": "Belarusian"
+    },
+    "bn": {
+        "group": "Indo-European",
+        "language": "Bengali"
+    },
+    "bh": {
+        "group": "Indo-European",
+        "language": "Bihari languages"
+    },
+    "bi": {
+        "group": "Creole",
+        "language": "Bislama"
+    },
+    "bs": {
+        "group": "Indo-European",
+        "language": "Bosnian"
+    },
+    "br": {
+        "group": "Indo-European",
+        "language": "Breton"
+    },
+    "bg": {
+        "group": "Indo-European",
+        "language": "Bulgarian"
+    },
+    "my": {
+        "group": "Sino-Tibetan",
+        "language": "Burmese"
+    },
+    "ca": {
+        "group": "Indo-European",
+        "language": "Catalan, Valencian"
+    },
+    "ch": {
+        "group": "Austronesian",
+        "language": "Chamorro"
+    },
+    "ce": {
+        "group": "Northeast Caucasian",
+        "language": "Chechen"
+    },
+    "ny": {
+        "group": "Niger–Congo",
+        "language": "Chichewa, Chewa, Nyanja"
+    },
+    "zh": {
+        "group": "Sino-Tibetan",
+        "language": "Chinese"
+    },
+    "cv": {
+        "group": "Turkic",
+        "language": "Chuvash"
+    },
+    "kw": {
+        "group": "Indo-European",
+        "language": "Cornish"
+    },
+    "co": {
+        "group": "Indo-European",
+        "language": "Corsican"
+    },
+    "cr": {
+        "group": "Algonquian",
+        "language": "Cree"
+    },
+    "hr": {
+        "group": "Indo-European",
+        "language": "Croatian"
+    },
+    "cs": {
+        "group": "Indo-European",
+        "language": "Czech"
+    },
+    "da": {
+        "group": "Indo-European",
+        "language": "Danish"
+    },
+    "dv": {
+        "group": "Indo-European",
+        "language": "Divehi, Dhivehi, Maldivian"
+    },
+    "nl": {
+        "group": "Indo-European",
+        "language": "Dutch, Flemish"
+    },
+    "dz": {
+        "group": "Sino-Tibetan",
+        "language": "Dzongkha"
+    },
+    "en": {
+        "group": "Indo-European",
+        "language": "English"
+    },
+    "eo": {
+        "group": "Constructed",
+        "language": "Esperanto"
+    },
+    "et": {
+        "group": "Uralic",
+        "language": "Estonian"
+    },
+    "ee": {
+        "group": "Niger–Congo",
+        "language": "Ewe"
+    },
+    "fo": {
+        "group": "Indo-European",
+        "language": "Faroese"
+    },
+    "fj": {
+        "group": "Austronesian",
+        "language": "Fijian"
+    },
+    "fl": {
+        "group": "Austronesian",
+        "language": "Filipino"
+    },
+    "fi": {
+        "group": "Uralic",
+        "language": "Finnish"
+    },
+    "fr": {
+        "group": "Indo-European",
+        "language": "French"
+    },
+    "ff": {
+        "group": "Niger–Congo",
+        "language": "Fulah"
+    },
+    "gl": {
+        "group": "Indo-European",
+        "language": "Galician"
+    },
+    "ka": {
+        "group": "South Caucasian",
+        "language": "Georgian"
+    },
+    "de": {
+        "group": "Indo-European",
+        "language": "German"
+    },
+    "el": {
+        "group": "Indo-European",
+        "language": "Greek (modern)"
+    },
+    "gn": {
+        "group": "Tupian",
+        "language": "Guaraní"
+    },
+    "gu": {
+        "group": "Indo-European",
+        "language": "Gujarati"
+    },
+    "ht": {
+        "group": "Creole",
+        "language": "Haitian, Haitian Creole"
+    },
+    "ha": {
+        "group": "Afro-Asiatic",
+        "language": "Hausa"
+    },
+    "he": {
+        "group": "Afro-Asiatic",
+        "language": "Hebrew (modern)"
+    },
+    "hz": {
+        "group": "Niger–Congo",
+        "language": "Herero"
+    },
+    "hi": {
+        "group": "Indo-European",
+        "language": "Hindi"
+    },
+    "ho": {
+        "group": "Austronesian",
+        "language": "Hiri Motu"
+    },
+    "hu": {
+        "group": "Uralic",
+        "language": "Hungarian"
+    },
+    "ia": {
+        "group": "Constructed",
+        "language": "Interlingua"
+    },
+    "id": {
+        "group": "Austronesian",
+        "language": "Indonesian"
+    },
+    "ie": {
+        "group": "Constructed",
+        "language": "Interlingue"
+    },
+    "ga": {
+        "group": "Indo-European",
+        "language": "Irish"
+    },
+    "ig": {
+        "group": "Niger–Congo",
+        "language": "Igbo"
+    },
+    "ik": {
+        "group": "Eskimo–Aleut",
+        "language": "Inupiaq"
+    },
+    "io": {
+        "group": "Constructed",
+        "language": "Ido"
+    },
+    "is": {
+        "group": "Indo-European",
+        "language": "Icelandic"
+    },
+    "it": {
+        "group": "Indo-European",
+        "language": "Italian"
+    },
+    "iu": {
+        "group": "Eskimo–Aleut",
+        "language": "Inuktitut"
+    },
+    "ja": {
+        "group": "Japonic",
+        "language": "Japanese"
+    },
+    "jv": {
+        "group": "Austronesian",
+        "language": "Javanese"
+    },
+    "kl": {
+        "group": "Eskimo–Aleut",
+        "language": "Kalaallisut, Greenlandic"
+    },
+    "kn": {
+        "group": "Dravidian",
+        "language": "Kannada"
+    },
+    "kr": {
+        "group": "Nilo-Saharan",
+        "language": "Kanuri"
+    },
+    "ks": {
+        "group": "Indo-European",
+        "language": "Kashmiri"
+    },
+    "kk": {
+        "group": "Turkic",
+        "language": "Kazakh"
+    },
+    "km": {
+        "group": "Austroasiatic",
+        "language": "Central Khmer"
+    },
+    "ki": {
+        "group": "Niger–Congo",
+        "language": "Kikuyu, Gikuyu"
+    },
+    "rw": {
+        "group": "Niger–Congo",
+        "language": "Kinyarwanda"
+    },
+    "ky": {
+        "group": "Turkic",
+        "language": "Kirghiz, Kyrgyz"
+    },
+    "kv": {
+        "group": "Uralic",
+        "language": "Komi"
+    },
+    "kg": {
+        "group": "Niger–Congo",
+        "language": "Kongo"
+    },
+    "ko": {
+        "group": "Koreanic",
+        "language": "Korean"
+    },
+    "ku": {
+        "group": "Indo-European",
+        "language": "Kurdish"
+    },
+    "kj": {
+        "group": "Niger–Congo",
+        "language": "Kuanyama, Kwanyama"
+    },
+    "la": {
+        "group": "Indo-European",
+        "language": "Latin"
+    },
+    "lb": {
+        "group": "Indo-European",
+        "language": "Luxembourgish, Letzeburgesch"
+    },
+    "lg": {
+        "group": "Niger–Congo",
+        "language": "Ganda"
+    },
+    "li": {
+        "group": "Indo-European",
+        "language": "Limburgan, Limburger, Limburgish"
+    },
+    "ln": {
+        "group": "Niger–Congo",
+        "language": "Lingala"
+    },
+    "lo": {
+        "group": "Tai–Kadai",
+        "language": "Lao"
+    },
+    "lt": {
+        "group": "Indo-European",
+        "language": "Lithuanian"
+    },
+    "lu": {
+        "group": "Niger–Congo",
+        "language": "Luba-Katanga"
+    },
+    "lv": {
+        "group": "Indo-European",
+        "language": "Latvian"
+    },
+    "gv": {
+        "group": "Indo-European",
+        "language": "Manx"
+    },
+    "mk": {
+        "group": "Indo-European",
+        "language": "Macedonian"
+    },
+    "mg": {
+        "group": "Austronesian",
+        "language": "Malagasy"
+    },
+    "ms": {
+        "group": "Austronesian",
+        "language": "Malay"
+    },
+    "ml": {
+        "group": "Dravidian",
+        "language": "Malayalam"
+    },
+    "mt": {
+        "group": "Afro-Asiatic",
+        "language": "Maltese"
+    },
+    "mi": {
+        "group": "Austronesian",
+        "language": "Maori"
+    },
+    "mr": {
+        "group": "Indo-European",
+        "language": "Marathi"
+    },
+    "mh": {
+        "group": "Austronesian",
+        "language": "Marshallese"
+    },
+    "mn": {
+        "group": "Mongolic",
+        "language": "Mongolian"
+    },
+    "na": {
+        "group": "Austronesian",
+        "language": "Nauru"
+    },
+    "nv": {
+        "group": "Dené–Yeniseian",
+        "language": "Navajo, Navaho"
+    },
+    "nd": {
+        "group": "Niger–Congo",
+        "language": "North Ndebele"
+    },
+    "ne": {
+        "group": "Indo-European",
+        "language": "Nepali"
+    },
+    "ng": {
+        "group": "Niger–Congo",
+        "language": "Ndonga"
+    },
+    "nb": {
+        "group": "Indo-European",
+        "language": "Norwegian Bokmål"
+    },
+    "nn": {
+        "group": "Indo-European",
+        "language": "Norwegian Nynorsk"
+    },
+    "no": {
+        "group": "Indo-European",
+        "language": "Norwegian"
+    },
+    "ii": {
+        "group": "Sino-Tibetan",
+        "language": "Sichuan Yi, Nuosu"
+    },
+    "nr": {
+        "group": "Niger–Congo",
+        "language": "South Ndebele"
+    },
+    "oc": {
+        "group": "Indo-European",
+        "language": "Occitan"
+    },
+    "oj": {
+        "group": "Algonquian",
+        "language": "Ojibwa"
+    },
+    "cu": {
+        "group": "Indo-European",
+        "language": "Church Slavic, Church Slavonic, Old Church Slavonic, Old Slavonic, Old Bulgarian"
+    },
+    "om": {
+        "group": "Afro-Asiatic",
+        "language": "Oromo"
+    },
+    "or": {
+        "group": "Indo-European",
+        "language": "Oriya"
+    },
+    "os": {
+        "group": "Indo-European",
+        "language": "Ossetian, Ossetic"
+    },
+    "pa": {
+        "group": "Indo-European",
+        "language": "Panjabi, Punjabi"
+    },
+    "pi": {
+        "group": "Indo-European",
+        "language": "Pali"
+    },
+    "fa": {
+        "group": "Indo-European",
+        "language": "Persian"
+    },
+    "pl": {
+        "group": "Indo-European",
+        "language": "Polish"
+    },
+    "ps": {
+        "group": "Indo-European",
+        "language": "Pashto, Pushto"
+    },
+    "pt": {
+        "group": "Indo-European",
+        "language": "Portuguese"
+    },
+    "qu": {
+        "group": "Quechuan",
+        "language": "Quechua"
+    },
+    "rm": {
+        "group": "Indo-European",
+        "language": "Romansh"
+    },
+    "rn": {
+        "group": "Niger–Congo",
+        "language": "Rundi"
+    },
+    "ro": {
+        "group": "Indo-European",
+        "language": "Romanian, Moldavian, Moldovan"
+    },
+    "ru": {
+        "group": "Indo-European",
+        "language": "Russian"
+    },
+    "sa": {
+        "group": "Indo-European",
+        "language": "Sanskrit"
+    },
+    "sc": {
+        "group": "Indo-European",
+        "language": "Sardinian"
+    },
+    "sd": {
+        "group": "Indo-European",
+        "language": "Sindhi"
+    },
+    "se": {
+        "group": "Uralic",
+        "language": "Northern Sami"
+    },
+    "sm": {
+        "group": "Austronesian",
+        "language": "Samoan"
+    },
+    "sg": {
+        "group": "Creole",
+        "language": "Sango"
+    },
+    "sr": {
+        "group": "Indo-European",
+        "language": "Serbian"
+    },
+    "gd": {
+        "group": "Indo-European",
+        "language": "Gaelic, Scottish Gaelic"
+    },
+    "sn": {
+        "group": "Niger–Congo",
+        "language": "Shona"
+    },
+    "si": {
+        "group": "Indo-European",
+        "language": "Sinhala, Sinhalese"
+    },
+    "sk": {
+        "group": "Indo-European",
+        "language": "Slovak"
+    },
+    "sl": {
+        "group": "Indo-European",
+        "language": "Slovenian"
+    },
+    "so": {
+        "group": "Afro-Asiatic",
+        "language": "Somali"
+    },
+    "st": {
+        "group": "Niger–Congo",
+        "language": "Southern Sotho"
+    },
+    "es": {
+        "group": "Indo-European",
+        "language": "Spanish, Castilian"
+    },
+    "su": {
+        "group": "Austronesian",
+        "language": "Sundanese"
+    },
+    "sw": {
+        "group": "Niger–Congo",
+        "language": "Swahili"
+    },
+    "ss": {
+        "group": "Niger–Congo",
+        "language": "Swati"
+    },
+    "sv": {
+        "group": "Indo-European",
+        "language": "Swedish"
+    },
+    "ta": {
+        "group": "Dravidian",
+        "language": "Tamil"
+    },
+    "te": {
+        "group": "Dravidian",
+        "language": "Telugu"
+    },
+    "tg": {
+        "group": "Indo-European",
+        "language": "Tajik"
+    },
+    "th": {
+        "group": "Tai–Kadai",
+        "language": "Thai"
+    },
+    "ti": {
+        "group": "Afro-Asiatic",
+        "language": "Tigrinya"
+    },
+    "bo": {
+        "group": "Sino-Tibetan",
+        "language": "Tibetan"
+    },
+    "tk": {
+        "group": "Turkic",
+        "language": "Turkmen"
+    },
+    "tl": {
+        "group": "Austronesian",
+        "language": "Tagalog"
+    },
+    "tn": {
+        "group": "Niger–Congo",
+        "language": "Tswana"
+    },
+    "to": {
+        "group": "Austronesian",
+        "language": "Tonga (Tonga Islands)"
+    },
+    "tr": {
+        "group": "Turkic",
+        "language": "Turkish"
+    },
+    "ts": {
+        "group": "Niger–Congo",
+        "language": "Tsonga"
+    },
+    "tt": {
+        "group": "Turkic",
+        "language": "Tatar"
+    },
+    "tw": {
+        "group": "Niger–Congo",
+        "language": "Twi"
+    },
+    "ty": {
+        "group": "Austronesian",
+        "language": "Tahitian"
+    },
+    "ug": {
+        "group": "Turkic",
+        "language": "Uighur, Uyghur"
+    },
+    "uk": {
+        "group": "Indo-European",
+        "language": "Ukrainian"
+    },
+    "ur": {
+        "group": "Indo-European",
+        "language": "Urdu"
+    },
+    "uz": {
+        "group": "Turkic",
+        "language": "Uzbek"
+    },
+    "ve": {
+        "group": "Niger–Congo",
+        "language": "Venda"
+    },
+    "vi": {
+        "group": "Austroasiatic",
+        "language": "Vietnamese"
+    },
+    "vo": {
+        "group": "Constructed",
+        "language": "Volapük"
+    },
+    "wa": {
+        "group": "Indo-European",
+        "language": "Walloon"
+    },
+    "cy": {
+        "group": "Indo-European",
+        "language": "Welsh"
+    },
+    "wo": {
+        "group": "Niger–Congo",
+        "language": "Wolof"
+    },
+    "fy": {
+        "group": "Indo-European",
+        "language": "Western Frisian"
+    },
+    "xh": {
+        "group": "Niger–Congo",
+        "language": "Xhosa"
+    },
+    "yi": {
+        "group": "Indo-European",
+        "language": "Yiddish"
+    },
+    "yo": {
+        "group": "Niger–Congo",
+        "language": "Yoruba"
+    },
+    "za": {
+        "group": "Tai–Kadai",
+        "language": "Zhuang, Chuang"
+    },
+    "zu": {
+        "group": "Niger–Congo",
+        "language": "Zulu"
+    },
+}
